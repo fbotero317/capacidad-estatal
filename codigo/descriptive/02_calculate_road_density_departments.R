1649576a4f73bf4aff39f2babbab2ea56d06b727
Gustavo A. Castillo A <120594773+guscastilloa@users.noreply.github.com>
2024-10-16 11:35:48 -0500

    calc pop density by dpto

--- codigo/descriptive/02_calculate_road_density_departments.R
+++ codigo/descriptive/02_calculate_road_density_departments.R
@@ -71,12 +71,35 @@ col_dpto <- col_dpto %>%
   left_join(st_drop_geometry(road_lengths_per_dept), by = "dpto_ccdgo") %>% 
   mutate(road_density = total_road_length_km / area_km2)
 
-# library(ggplot2)
-# 
-# ggplot(data = col_dpto) +
-#   geom_sf(aes(fill = road_density)) +
-#   scale_fill_viridis_c() +
-#   labs(title = "Road Density by Department in Colombia", fill = "Road Density\n(km/km²)")
+
+#===================================#
+# 4. Population density dpto ------
+#===================================#
+if(!file.exists("datos/spatial/pop_dpto_intersect.rds")){
+  # Prepare parallelization
+  plan(multisession, workers = parallel::detectCores()-1)  
+  
+  intersections <- st_intersects(x = pop_hex, y = col_dpto)
+  
+  # Initialize progress bar
+  pb <- progress::progress_bar$new(format = "[:bar] :current/:total (:percent)", total = dim(roads_colombia)[1])
+  
+  # Define parallelized intersection function
+  intersectFeatures <- future_map_dfr(1:dim(pop_hex)[1], function(ix){
+    pb$tick()
+    st_intersection(x = pop_hex[ix,], y = col_dpto[intersections[[ix]],])
+  })
+  plan(sequential)  # Reset the plan to the default single-core mode
+  saveRDS(intersectFeatures, 
+          file ="datos/spatial/pop_dpto_intersect.rds" ) 
+  pop_by_department <- intersectFeatures
+  
+} else{
+  
+  pop_by_department <- readRDS("datos/spatial/pop_dpto_intersect.rds")
+}
+
+
 
 # 5. Export -----
 out <- col_dpto %>% 
